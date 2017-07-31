360cfb98a4ef99fc4e4d90a346920670545dc544
Will Shaw <wrcs505@gmail.com>
2017-07-31 18:51:46 -0400

    last day 1 1commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
+const chalk = require('chalk');
+
+app.use(function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+    console.log(chalk.blue('new request', req.method, req.url));
+    console.log(chalk.green('new response', res.method, res.url));
+    next();
+});
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
