0f2eebfbbe3b5738988faec12eaf08e7c84c23af
Jess Freasier <jfreasier@gmail.com>
2017-08-01 12:04:17 -0500

    up to 7. Static Routing

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const chalk = require('chalk');
-const nunjucks = require('nunjucks')
+const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+
 
 app.set('view engine', 'html'); // have res.render work with html files
 app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
@@ -10,7 +13,9 @@ nunjucks.configure('views', {
 
 }); // point nunjucks to the proper directory for templates
 
+app.use('/', routes);
 
+/*
 app.use(function (req, res, next) {
   // do your logging here
   // call `next`, or else your app will be a black hole — receiving requests but never properly responding
@@ -19,16 +24,21 @@ app.use(function (req, res, next) {
   next();
 });
 
-
 app.get('/', function (req, res) {
+
   const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
   res.render( 'index', {title: 'Hall of Fame', people: people} );
+
 });
 
 app.get('/news', function (req, res) {
+
   res.send('The news: swag.');
+
 });
 
+*/
 app.listen(3000, function () {
 	console.log('Listening patiently at port 3000');
 });
+
