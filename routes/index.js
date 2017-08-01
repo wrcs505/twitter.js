f08f926b12257cf6957ca2ce145b32a8094dcf94
Will Shaw <wrcs505@gmail.com>
2017-08-01 15:43:05 -0400

    some stuff work not so wells

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,16 @@ router.get('/', function (req, res) {
   res.render( 'index', { tweets: tweets } );
 });
 
+router.get( '/users/:name', function (req, res) {
+  // let user = tweetBank.find({req.params.name, req.params.content});
+  console.log(Array.isArray(tweetBank.list()));
+  let tweets = tweetBank.list().forEach(function(tweetObj) {
+    // console.log(tweetObj)
+    if (tweetObj.name === req.params.name) return tweetObj;
+  });
+  console.log(tweets);
+  res.render( 'index', { tweets: tweets } );
+  // console.log( req.params.content ); // --> 'nimit'
+});
+
 module.exports = router;
