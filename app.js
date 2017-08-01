6206407b7463eda178b990d8be4f0f32f1a85c7a
Will Shaw <wrcs505@gmail.com>
2017-08-01 11:21:12 -0400

    njk integration done

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 const chalk = require('chalk');
+const nunjucks = require('nunjucks')
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {
+  noCache: true,
+
+}); // point nunjucks to the proper directory for templates
+
 
 app.use(function (req, res, next) {
-    // do your logging here
-    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
-    console.log(chalk.blueBright('new request', req.method, req.url));
-    console.log(chalk.green('new response', res.method, res.url));
-    next();
+  // do your logging here
+  // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+  console.log(chalk.blueBright('new request', req.method, req.url));
+  console.log(chalk.green('new response', res.method, res.url));
+  next();
 });
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+  res.render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.get('/news', function (req, res) {
-  res.send('The news: swag.')
+  res.send('The news: swag.');
 });
 
 app.listen(3000, function () {
